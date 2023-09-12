0d5d359106a0920bd5c0c0f28eec19e4bc16493f
natalia <nataliapit0406@gmail.com>
2023-09-12 20:21:54 +0300

    исправление первых заданий

--- src/work2.java
+++ src/work2.java
@@ -1,12 +1,9 @@
 public class work2 {
-    public static void main(String[] args) {
-        System.out.println(convert(5));
-
-
-
+    public static void main(String args[]) {
+        System.out.println(duplicateChars(String str = "malayalam"));
 
     }
-    public static float convert (int x){
+    public static float duplicateChars (int x){
         return x * 3.785f;
     }
 

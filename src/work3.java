ba36ede19cacd1eebb345d777070800f10f73eec
natalia <nataliapit0406@gmail.com>
2023-10-09 21:34:09 +0300

    доработка дз 3(01)

--- src/work3.java
+++ src/work3.java
@@ -1,5 +1,6 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Arrays;
 
 
 public class work3 {
@@ -10,9 +11,9 @@ public class work3 {
         System.out.println("задание2: " + stringTransform("heDoubleLo"));
         System.out.println(stringTransform("bookkeeper"));
 
-        //System.out.println("задание3: " + doesBlockkFit(1, 3, 5, 4, 5));
-        //System.out.println(doesBlockkFit(1, 8, 1, 1, 1));
-        // System.out.println(doesBlockkFit(1, 2, 2, 1, 1));
+        System.out.println("задание3: " + doesBlockkFit(1, 3, 5, 4, 5));
+        System.out.println(doesBlockkFit(1, 8, 1, 1, 1));
+        System.out.println(doesBlockkFit(1, 2, 2, 1, 1));
 
         System.out.println("задание4:" + numCheck(243));
         System.out.println(numCheck(52));
@@ -21,14 +22,15 @@ public class work3 {
         System.out.println(countRoots(new int[]{2, 5, 2}));
         System.out.println(countRoots(new int[]{1, -6, 9}));
 
-        // System.out.println("задание6:" + salesData( new String[][]{
-        //  {"Apple", "Shop1", "Shop2", "Shop3", "Shop4"},
-        // {"Banana", "Shop2", "Shop3", "Shop4"},
-        //  {"Orange", "Shop1", "Shop3", "Shop4"},
-        //   {"Pear", "Shop2", "Shop4"}}));
+        System.out.println("задание6:" + Arrays.toString(salesData(new String[][]{
+                {"Apple", "Shop1", "Shop2", "Shop3", "Shop4"},
+                {"Banana", "Shop2", "Shop3", "Shop4"},
+                {"Orange", "Shop1", "Shop3", "Shop4"},
+                {"Pear", "Shop2", "Shop4"}})));
 
         System.out.println("задание7:" + validSplit("apple eagle egg goat"));
         System.out.println(validSplit("cat dog goose fish"));
+        System.out.println(validSplit("cat Town"));
 
         System.out.println("задание8" + waveForm(new int[]{3, 1, 4, 2, 7, 5}));
         System.out.println(waveForm(new int[]{1, 2, 3, 4, 5}));
@@ -37,8 +39,13 @@ public class work3 {
         System.out.println("задание9: " + commonVovel("Hello world"));
         System.out.println(commonVovel("Actions speak louder than words."));
 
+        System.out.println("задание10: " + Arrays.deepToString(dataScience(new int[][]{
+                {6, 4, 19, 0, 0},
+                {81, 25, 3, 1, 17},
+                {48, 12, 60, 32, 14},
+                {91, 47, 16, 65, 217},
+                {5, 73, 0, 4, 21}})));
     }
-
 
     //задание 1
     public static String duplicateChars(String str) {
@@ -53,8 +60,16 @@ public class work3 {
         return (result);
     }
 
-    //задание 3
+    //задание 3 (разобрать)
+    static boolean doesBlockkFit(int a, int b, int c, int w, int h) {
+        int[] kubik = {a, b, c};
+        int[] otverstie = {w, h};
 
+        Arrays.sort(kubik);
+        Arrays.sort(otverstie);
+
+        return kubik[0] <= otverstie[0] && kubik[1] <= otverstie[1];
+    }
 
     //задание 4
     public static boolean numCheck(int x) {
@@ -94,8 +109,25 @@ public class work3 {
 
 
     }
-    //задание 6 разобраться с двумерными массивами
-    //public static salesData(String[][] arr)
+
+    //задание 6 разобраться с двумерными массивами(!)
+    public static String[] salesData(String[][] data) {
+        int maxLen = 0;
+        for (String[] row : data) {
+            if (row.length > maxLen) {
+                maxLen = row.length;
+            }
+        }
+        int count = 0;
+        String[] result = new String[data.length];
+        for (String[] row : data) {
+            if (row.length == maxLen) {
+                result[count] = row[0];
+                count++;
+            }
+        }
+        return Arrays.copyOf(result, count);
+    }
 
 
     //задание 7
@@ -113,9 +145,13 @@ public class work3 {
         return true;
     }
 
-    //задание 8 переделать
+    //задание 8 переделать(можно сделать проще взяв другой флаг)
 
     public static boolean waveForm(int[] arr) {
+
+        if (arr.length < 3)
+            return false;
+
         boolean increasing = true;
         for (int i = 0; i < arr.length - 1; i++) {
             if ((increasing && arr[i] <= arr[i + 1]) || (!increasing && arr[i] >= arr[i + 1])) {
@@ -126,36 +162,50 @@ public class work3 {
         return true;
     }
 
-        //задание 9 переделать
-        public static char commonVovel(String str){
-            String sentence = str.toLowerCase();
-            Map<Character, Integer> vowelCount = new HashMap<>();
-             String vowels = "aeiou"; // Гласные буквы
+    //задание 9 переделать(тоже можно переделать)
+    public static char commonVovel(String str) {
+        String sentence = str.toLowerCase();
+        Map<Character, Integer> vowelCount = new HashMap<>();
+        String vowels = "aeiou"; // Гласные буквы
 
-              for (char vowel : vowels.toCharArray()) {
+        for (char vowel : vowels.toCharArray()) {
             vowelCount.put(vowel, 0);
-    }
+        }
 
-            for (char letter : sentence.toCharArray()) {
+        for (char letter : sentence.toCharArray()) {
             if (vowelCount.containsKey(letter)) {
-            vowelCount.put(letter, vowelCount.get(letter) + 1);
-             }
+                vowelCount.put(letter, vowelCount.get(letter) + 1);
             }
-            char mostCommonVowel = ' ';
-            int maxCount = 0;
+        }
+        char mostCommonVowel = ' ';
+        int maxCount = 0;
 
-            for (char vowel : vowels.toCharArray()) {
-             int count = vowelCount.get(vowel);
+        for (char vowel : vowels.toCharArray()) {
+            int count = vowelCount.get(vowel);
             if (count > maxCount) {
-            maxCount = count;
-            mostCommonVowel = vowel;
+                maxCount = count;
+                mostCommonVowel = vowel;
             }
         }
 
         return mostCommonVowel;
     }
 
-        //задание 10
+    //задание 10
+    public static int[][] dataScience(int[][] arrays) {
+        int n = arrays.length;
+        for (int i = 0; i < n; i++) {
+            int total = 0;
+            for (int k = 0; k < n; k++) {
+                if (k != i) {
+                    total += arrays[k][i];
+                }
+            }
+            arrays[i][i] = total / (n - 1);
+        }
+        return arrays;
+    }
 
 
 }
+
